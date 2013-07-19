c797a4960c4d6635e4cc3f041e51ffd723197e4c
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-07-19 12:46:07 +0300

    Moving old code to old_utils

---
+++ old_utils/post-transifix.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Fixes the translation folders after you pull them in from Transifex…
+ */
+
+function fixitup($dir) {
+	echo "\t$dir\n";
+	
+	$allLangs = array();
+	$dh = opendir($dir);
+	while($subdir = readdir($dh)) {
+		if(substr($subdir,0,1) == '.') continue;
+		if(!is_dir($dir.'/'.$subdir)) continue;
+		if(substr($subdir, 2, 1) == '-') continue;
+		
+		$allLangs[] = $subdir;
+	}
+	closedir($dh);
+	
+	foreach($allLangs as $tLang) {
+		$jLang = str_replace('_', '-', $tLang);
+		$sourceDir = $dir.'/'.$tLang;
+		$destDir = $dir.'/'.$jLang;
+		if(!is_dir($destDir)) mkdir($destDir);
+		
+		$dh = opendir($sourceDir);
+		while($file = readdir($dh)) {
+			if(!is_file($sourceDir.'/'.$file)) continue;
+			
+			$sourceFile = $sourceDir.'/'.$file;
+			$destFile = $destDir.'/'.str_replace($tLang, $jLang, $file);
+			copy($sourceFile, $destFile);
+			unlink($sourceFile);
+		}
+		closedir($dh);
+		rmdir($sourceDir);
+	}
+}
+
+$root = dirname(__FILE__).'/translations';
+// Fix component language files
+echo "Fixing component translations\n";
+$dh = opendir($root.'/component');
+while($file = readdir($dh)) {
+	if(substr($file,0,1) == '.') continue;
+	if(!is_dir($root.'/component/'.$file)) continue;
+
+	fixitup($root.'/component/'.$file);
+}
+closedir($dh);
+
+// Fix module language files
+echo "Fixing modules translations\n";
+$dh = opendir($root.'/modules');
+while($file = readdir($dh)) {
+	if(substr($file,0,1) == '.') continue;
+	if(!is_dir($root.'/modules/'.$file)) continue;
+	
+	$dh2 = opendir($root.'/modules/'. $file);
+	while($subfolder = readdir($dh2)) {
+		if(substr($subfolder,0,1) == '.') continue;
+		if(!is_dir($root.'/modules/'. $file.'/'.$subfolder)) continue;
+		
+		fixitup($root.'/modules/'. $file.'/'.$subfolder);
+	}
+	closedir($dh2);
+}
+closedir($dh);
+
+// Fix plugin language files
+echo "Fixing plugins translations\n";
+$dh = opendir($root.'/plugins');
+while($file = readdir($dh)) {
+	if(substr($file,0,1) == '.') continue;
+	if(!is_dir($root.'/plugins/'.$file)) continue;
+	
+	$dh2 = opendir($root.'/plugins/'. $file);
+	while($subfolder = readdir($dh2)) {
+		if(substr($subfolder,0,1) == '.') continue;
+		if(!is_dir($root.'/plugins/'. $file.'/'.$subfolder)) continue;
+		
+		fixitup($root.'/plugins/'. $file.'/'.$subfolder);
+	}
+	closedir($dh2);
+}
+closedir($dh);
---
+++ old_utils/transstats.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Produce translation statistics
+ */
+
+$translationPaths = array(
+	'component/translations/backend',
+	'component/translations/frontend',
+	'modules/mod_akadmin/translations',
+	'plugins/aklazy'
+);
+
+function getNumberOfKeys($filename)
+{
+	$fp = fopen($filename,'rt');
+	if($fp == false) return 0;
+	
+	$keys = 0;
+	
+	while(!feof($fp)) {
+		$line = fgets($fp);
+		$trimmed = trim($line);
+		if(empty($trimmed)) continue;
+		if(substr($trimmed,0,1) == ';') continue;
+		if(substr($trimmed,0,1) == '#') continue;
+		$keys++;
+	}
+	
+	fclose($fp);
+	
+	return $keys;
+}
+
+function getQualityMark($percentage)
+{
+	if($percentage < 50) {
+		return "Unacceptable";
+	} elseif($percentage < 60) {
+		return "Very Poor";
+	} elseif($percentage < 70) {
+		return "Poor";
+	} elseif($percentage < 80) {
+		return "Fair";
+	} elseif($percentage < 90) {
+		return "Good";
+	} elseif($percentage <= 99) {
+		return "Very Good";
+	} else {
+		return "Excellent";
+	}	
+}
+
+$langs = array();
+
+foreach($translationPaths as $root)
+{
+	echo "\nStatistics for $root\n";
+	
+	// Initialize
+	$stats = array();
+	
+	// Grab key stats from all files
+	$files = glob("$root/*.ini");
+	foreach($files as $filename) {
+		$base = basename($filename, '.ini');
+		list($language, $key) = explode('.', $base, 2);
+		$transKeys = getNumberOfKeys($filename);
+		
+		$stats[$key][$language] = $transKeys;
+		
+		if(!array_key_exists($language, $langs)) $langs[$language] = 0;
+	}
+	
+	// Show stats for each key
+	foreach($stats as $key => $table)
+	{
+		asort($table);
+		if(!array_key_exists('en-GB', $table)) continue;
+		echo "\t$key :\n";
+		$ref = $table['en-GB'];
+		$langs['en-GB'] += $ref;
+		foreach($table as $language => $keys) {
+			if($language == 'en-GB') continue;
+			$langs[$language] += $keys;
+			$percentage = (int)(100*($keys/$ref));
+			if( ($percentage == 100) && ($keys < $ref) ) $percentage = 99;
+			$xofx = sprintf('%03u of %03u', $keys, $ref);
+			echo "\t\t$language\t$xofx\t[$percentage%]\t".getQualityMark($percentage)."\n";
+		}
+	}
+}
+
+echo "\n\n-------------------------------------------------------------------------------\nOVERALL TRANSLATION STATISTICS\n-------------------------------------------------------------------------------\n";
+$totalLangs = count($langs);
+
+$ref = $langs['en-GB'];
+unset($langs['en-GB']);
+
+asort($langs);
+
+$totalKeys = 0;
+foreach($langs as $language => $keys)
+{
+	$totalKeys += $keys;
+	$percentage = (int)(100*($keys/$ref));
+	if( ($percentage == 100) && ($keys < $ref) ) $percentage = 99;
+	$xofx = sprintf('%03u of %03u', $keys, $ref);
+	echo "\t$language\t$xofx\t[$percentage%]\t".getQualityMark($percentage)."\n";
+}
+
+$avgKeys = $totalKeys / ($totalLangs-1);
+$avgQuality = 100* $avgKeys / $keys;
+
+echo "\nTOTAL LANGUAGES : $totalLangs\n";
+echo "AVERAGE QUALITY : " . sprintf('%02u', $avgQuality).'% ('.getQualityMark($avgQuality).")\n\n";
