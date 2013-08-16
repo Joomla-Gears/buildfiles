fed51423d9ae0ea033767e275f13d963156c1e2d
Jurian Even <jurian@twentronix.com>
2013-08-16 14:43:26 +0200

    Add date format option to GitDateTask

--- phingext/GitDateTask.php
+++ phingext/GitDateTask.php
@@ -4,44 +4,84 @@ require_once 'phing/tasks/ext/svn/SvnBaseTask.php';
 
 class GitDateTask extends SvnBaseTask
 {
-    private $propertyName = "git.date";
+	/**
+	 * Git.date
+	 *
+	 * @var 	string
+	 */
+	private $propertyName = "git.date";
 
-    /**
-     * Sets the name of the property to use
-     */
-    function setPropertyName($propertyName)
-    {
-        $this->propertyName = $propertyName;
-    }
+	/**
+	 * The date format. Uses Unix timestamp by default.
+	 *
+	 * @var 	string
+	 *
+	 * @see		http://www.php.net/manual/en/function.date.php
+	 */
+	private $format = 'U';
 
-    /**
-     * Returns the name of the property to use
-     */
-    function getPropertyName()
-    {
-        return $this->propertyName;
-    }
+	/**
+	 * The working copy.
+	 *
+	 * @var		string
+	 */
+	private $workingCopy;
 
-    /**
-     * Sets the path to the working copy
-     */
-    function setWorkingCopy($wc)
-    {
-        $this->workingCopy = $wc;
-    }
-    
-    /**
-     * The main entry point
-     *
-     * @throws BuildException
-     */
-    function main()
-    {
+	/**
+	 * Sets the name of the property to use
+	 */
+	function setPropertyName($propertyName)
+	{
+		$this->propertyName = $propertyName;
+	}
+
+	/**
+	 * Returns the name of the property to use
+	 */
+	function getPropertyName()
+	{
+		return $this->propertyName;
+	}
+
+	/**
+	 * Sets the path to the working copy
+	 */
+	function setWorkingCopy($wc)
+	{
+		$this->workingCopy = $wc;
+	}
+
+	/**
+	 * Gets the date format
+	 */
+	function getFormat()
+	{
+		return $this->format;
+	}
+
+	/**
+	 * Sets the date format
+	 *
+	 * @param 	$format
+	 */
+	function setFormat($format)
+	{
+		$this->format = $format;
+	}
+
+	/**
+	 * The main entry point
+	 *
+	 * @throws BuildException
+	 */
+	function main()
+	{
 		$this->setup('info');
-		
+
 		if($this->workingCopy == '..') $this->workingCopy = '../';
-		
-		exec('git log --format=%at -n1 '.escapeshellarg($this->workingCopy), $out);
-		$this->project->setProperty($this->getPropertyName(), strtoupper(trim($out[0])));
-    }
+
+		exec('git log --format=%at -n1 '.escapeshellarg($this->workingCopy), $timestamp);
+		$date = date($this->format, trim($timestamp[0]));
+		$this->project->setProperty($this->getPropertyName(), $date);
+	}
 }
