594ae11b9e48018bb1881b32363900af0db09d9d
Jurian Even <jurian@twentronix.com>
2014-03-19 12:21:38 +0100

    We don't need the radical approach anymore to prevent zipping of unwanted files
    See also:
    https://github.com/akeeba/buildfiles/commit/4b8b429217c850fe3842d5f2a1b7072ea12e3015

--- phingext/ZipmeTask.php
+++ phingext/ZipmeTask.php
@@ -108,7 +108,8 @@ class ZipmeTask extends MatchingTask
 	}
 
 	/**
-	 * do the work
+	 * Do the work
+	 *
 	 * @throws BuildException
 	 */
 	public function main()
@@ -151,67 +152,24 @@ class ZipmeTask extends MatchingTask
 
 			foreach ($this->filesets as $fs)
 			{
-				$files = array();
+				$files = $fs->getFiles($this->project, $this->includeEmpty);
 
-				$rawFiles = $fs->getFiles($this->project, $this->includeEmpty);
+				$fsBasedir = ($this->baseDir != null) ? $this->baseDir : $fs->getDir($this->project);
 
-				$fsBasedir = ($this->baseDir != null) ? $this->baseDir :
-					$fs->getDir($this->project);
+				$filesToZip = array();
 
-				// $rawFiles should only contain files. However... it could also contain
-				// a link to a directory in stead of a file! In such a case the filtering of files
-				// (which should not be included in the zip package) doesn't work. Therefore we
-				// have to iterate and check every file...
-				foreach ($rawFiles as $file)
+				foreach ($files as $file)
 				{
 					$f = new PhingFile($fsBasedir, $file);
 
 					$fileAbsolutePath = $f->getPath();
-
-					if (is_file($fileAbsolutePath))
-					{
-						$files[] = $fileAbsolutePath;
-					}
-					else if (is_dir($fileAbsolutePath))
-					{
-						// Say what?! The 'file' is actually a directory! Caused by symlinking
-						$dir = $fileAbsolutePath;
-
-						// Iterate over that directory to get the files and add them to $files
-						$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-						foreach ($iterator as $fileInfo)
-						{
-							if (!$fileInfo->isFile())
-							{
-								continue;
-							}
-
-							$files[] = $fileInfo->getPathname();
-						}
-					}
-					else if (is_link($fileAbsolutePath))
-					{
-						// This should never happen. If it does happen it's a bug
-						die('ZipmeTask error: there\'s a bug in ZipmeTask (line ' . __LINE__ . ').');
-					}
-					else
-					{
-						// This should never happen. It's not a file, dir or link
-						die('ZipmeTask error: unknown file type. See line' . __LINE__ . '.');
-					}
-				}
-
-				// Ok, we've got the files. Let's check every file if it needs to be included
-				// in the zip package. If that's the case then add it to the zip package.
-				$filesToZip = array();
-				foreach ($files as $fileAbsolutePath)
-				{
 					$fileDir = rtrim(dirname($fileAbsolutePath), '/\\');
 					$fileBase = basename($fileAbsolutePath);
 
 					// Only use lowercase for $disallowedBases because we'll convert $fileBase to lowercase
 					$disallowedBases = array('.ds_store', '.svn', '.gitignore', 'thumbs.db');
 					$fileBaseLower = strtolower($fileBase);
+
 					if (in_array($fileBaseLower, $disallowedBases))
 					{
 						continue;
@@ -221,6 +179,7 @@ class ZipmeTask extends MatchingTask
 					{
 						continue;
 					}
+
 					if (substr(rtrim($fileAbsolutePath,'/\\'), -4 ) == '.svn')
 					{
 						continue;
@@ -270,7 +229,9 @@ class ZipmeFileSet extends FileSet
 	{
 		parent::__construct($fileset);
 
-		// Expand/dereference symbolic links in order to also include nested symlinks to the zipfile
+		// Expand/dereference symbolic links in order to also include nested symlinks to the zipfile.
+		// This is also required in order to prevent zipping of unwanted files, else symlinked files
+		// are detected as directories and our checks within main() fails!
 		$this->setExpandSymbolicLinks(true);
 	}
 
