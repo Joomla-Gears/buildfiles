eb3f3323a059134eae94d180cf9b59335bd04f28
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-12-18 13:21:42 +0200

    YUI Compressor and Closure Compiler tasks

---
+++ phing/tasks/ClosureTask.php
@@ -0,0 +1,245 @@
+<?php
+
+class ClosureTask extends Task
+{
+	/**
+	 * Path to Closure Compiler's JAR file
+	 *
+	 * @var string
+	 */
+	protected $compilerPath = '';
+
+	/**
+	 * Closure Compiler compilation optimization level
+	 *
+	 * @var string
+	 */
+	protected $compilationLevel = 'SIMPLE_OPTIMIZATIONS';
+
+	/**
+	 * Should I generate a source map?
+	 *
+	 * @var bool
+	 */
+	protected $generateMap = true;
+
+	/**
+	 * Source files
+	 *
+	 * @var  FileSet[]
+	 */
+	protected $filesets = [];
+
+	/**
+	 * Should the build fail if Closure Compiler reports errors in the compilation?
+	 *
+	 * @var boolean
+	 */
+	protected $failonerror = false;
+
+	/**
+	 * Where should I save minified files?
+	 *
+	 * @var  string
+	 */
+	protected $targetPath = '';
+
+	public function __construct()
+	{
+		$this->compilerPath = __DIR__ . '/library/closure.jar';
+	}
+
+	/**
+	 * Adds a set of files (nested fileset attribute).
+	 */
+	public function createFileSet()
+	{
+		$num = array_push($this->filesets, new FileSet());
+
+		return $this->filesets[$num - 1];
+	}
+
+	/**
+	 * Should the build fail on error?
+	 *
+	 * @param boolean $value
+	 */
+	public function setFailonerror($value)
+	{
+		$this->failonerror = $value;
+	}
+
+	/**
+	 * Set the output path
+	 *
+	 * @param  string $targetPath
+	 */
+	public function setTargetPath($targetPath)
+	{
+		$this->targetPath = $targetPath;
+	}
+
+	/**
+	 * Path to Closure Compiler JAR
+	 *
+	 * @param  string $compilerPath
+	 */
+	public function setCompilerPath($compilerPath)
+	{
+		$this->compilerPath = $compilerPath;
+	}
+
+	/**
+	 * Compilation optimization level
+	 *
+	 * @param  string $level
+	 */
+	public function setCompilationLevel($level)
+	{
+		$validLevels = [
+			'WHITESPACE_ONLY',
+			'SIMPLE_OPTIMIZATIONS',
+			'ADVANCED_OPTIMIZATIONS',
+		];
+
+		$level = strtoupper($level);
+
+		if (!in_array($level, $validLevels))
+		{
+			throw new BuildException("Invalid compilation level '$level'. Must be one of WHITESPACE_ONLY, SIMPLE_OPTIMIZATIONS, ADVANCED_OPTIMIZATIONS");
+		}
+
+		$this->compilationLevel = $level;
+	}
+
+	/**
+	 * Should I generate a sourcemap?
+	 *
+	 * @param bool $value
+	 *
+	 * @return void
+	 */
+	public function setGenerateSourcemap($value)
+	{
+		$this->generateMap = in_array($value, [1, 'yes', 'true', true], true);
+	}
+
+	/**
+	 * Initialization
+	 */
+	public function init()
+	{
+		return true;
+	}
+
+	/**
+	 * The main entry point method.
+	 */
+	public function main()
+	{
+		if (count($this->filesets) == 0)
+		{
+			throw new BuildException("You must specify a 'file' attribute or a nested fileset");
+		}
+
+		$commandBase = 'java -jar ' . $this->compilerPath;
+		exec($commandBase . ' --helpshort 2>&1', $output);
+
+		if (!preg_match('/"--helpshort" is not a valid option/', implode('', $output)))
+		{
+			throw new BuildException('Closure Compiler not found!');
+		}
+
+		foreach ($this->filesets as $fs)
+		{
+			try
+			{
+				$files    = $fs->getDirectoryScanner($this->project)->getIncludedFiles();
+				$fullPath = realpath($fs->getDir($this->project));
+
+				if (empty($this->targetPath))
+				{
+					foreach ($files as $file)
+					{
+						$inputPath = $fullPath . '/' . $file;
+						$target    = $inputPath;
+
+						$this->compileJSFile($target, $inputPath);
+					}
+				}
+				else
+				{
+					$target = $this->targetPath;
+
+					if (file_exists(dirname($target)) === false)
+					{
+						mkdir(dirname($target), 0700, true);
+					}
+
+					foreach ($files as $file)
+					{
+						$inputPath = $fullPath . '/' . $file;
+
+						$this->compileJSFile($target . '/' . basename($file), $inputPath);
+					}
+				}
+			}
+			catch (BuildException $be)
+			{
+				// directory doesn't exist or is not readable
+				if ($this->failonerror)
+				{
+					throw $be;
+				}
+				else
+				{
+					$this->log($be->getMessage(), $this->quiet ? Project::MSG_VERBOSE : Project::MSG_WARN);
+				}
+			}
+		}
+	}
+
+	private function _exec($command)
+	{
+		$this->log('Minifying files with ' . $command);
+		exec($command . ' 2>&1', $output, $return);
+		if ($return > 0)
+		{
+			$out_string = implode("\n", $output);
+			$this->log("Error minifiying:\n{$command}\n{$out_string}", Project::MSG_ERR);
+			throw new BuildException('error in minification');
+		}
+	}
+
+	/**
+	 * @param $target
+	 * @param $inputPath
+	 */
+	protected function compileJSFile($target, $inputPath)
+	{
+		$commandBase = 'java -jar ' . $this->compilerPath;
+
+		if ((substr($target, -3) == '.js') && (substr($target, -7) != '.min.js'))
+		{
+			$target = substr($target, 0, -3) . '.min.js';
+		}
+
+		$command = $commandBase;
+		$command .= ' --js ' . $inputPath;
+		$command .= " --js_output_file " . $target;
+		$command .= " --compilation_level " . $this->compilationLevel;
+
+		if ($this->generateMap)
+		{
+			$mapTarget = substr($target, 0, -3) . '.map';
+			$command   .= ' --create_source_map ' . $mapTarget;
+
+			if (DIRECTORY_SEPARATOR != '\\')
+			{
+				$command .= ' --output_wrapper "%output% //# sourceMappingURL=' . basename($mapTarget) . '"';
+			}
+		}
+
+		$this->_exec($command);
+	}
+}
---
+++ phing/tasks/YuiCompressorTask.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Part of the Phing tasks collection by Ryan Chouinard.
+ *
+ * @author Ryan Chouinard <rchouinard@gmail.com>
+ * @copyright Copyright (c) 2010 Ryan Chouinard
+ * @license New BSD License
+ */
+
+/**
+ * Defines a Phing task to run the YUI compressor against a set of JavaScript
+ * or CSS files.
+ *
+ * A java binary must be available in the environment PATH for this task to
+ * work.
+ *
+ * To use this task, include it with a taskdef tag in your build.xml file:
+ *
+ *     <taskdef name="yuic" classname="my.tasks.YuiCompressorTask" />
+ *
+ * The task is now ready to be used:
+ *
+ *     <target name="yui-compressor" description="Compress CSS and JavaScript">
+ *         <yuic targetdir="path/to/target">
+ *             <fileset dir="path/to/source">
+ *                 <include name="*.css" />
+ *                 <include name="*.js" />
+ *             </fileset>
+ *         </yuic>
+ *     </target>
+ *
+ * This task makes use of the
+ * {@link http://developer.yahoo.com/yui/compressor/ YUI compressor}. Version
+ * 2.4.2 of the compiled jar file is bundled with this task, however a different
+ * jar file may be specified using the optional jarpath attribute.
+ */
+class YuiCompressorTask extends Task
+{
+
+	/**
+	 * @var string
+	 */
+	protected $_javaPath;
+
+	/**
+	 * @var PhingFile
+	 */
+	protected $_jarPath;
+
+	/**
+	 * @var PhingFile
+	 */
+	protected $_targetDir;
+
+	/**
+	 * @var array
+	 */
+	protected $_fileSets;
+
+	/**
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$defaultJarPath = realpath(
+			__DIR__ . '/library/yui-compressor.jar'
+		);
+
+		$this->_javaPath = 'java';
+		$this->_jarPath = new PhingFile($defaultJarPath);
+		$this->_fileSets = array ();
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function init()
+	{
+		return true;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function main()
+	{
+		$this->_checkJarPath();
+		$this->_checkTargetDir();
+
+		/* @var $fileSet FileSet */
+		foreach ($this->_fileSets as $fileSet) {
+
+			$files = $fileSet->getDirectoryScanner($this->project)
+				->getIncludedFiles();
+
+			foreach ($files as $file) {
+
+				$targetDir = new PhingFile($this->_targetDir, dirname($file));
+				if (!$targetDir->exists()) {
+					$targetDir->mkdirs();
+				}
+				unset ($targetDir);
+
+				$targetFilename = $file;
+
+				if ((substr($targetFilename, -4) == '.css') && (substr($targetFilename, -8) != '.min.css'))
+				{
+					$targetFilename = substr($targetFilename, 0, -4) . '.min.css';
+				}
+				elseif ((substr($targetFilename, -3) == '.js') && (substr($targetFilename, -7) != '.min.js'))
+				{
+					$targetFilename = substr($targetFilename, 0, -3) . '.min.js';
+				}
+
+				$source = new PhingFile($fileSet->getDir($this->project), $file);
+				$target = new PhingFile($this->_targetDir, $targetFilename);
+
+				$this->log("Processing ${file}");
+				$cmd = escapeshellcmd($this->_javaPath)
+					. ' -jar ' . escapeshellarg($this->_jarPath)
+					. ' -o ' . escapeshellarg($target->getAbsolutePath())
+					. ' ' . escapeshellarg($source->getAbsolutePath());
+				$this->log('Executing: ' . $cmd);
+				$this->log('Executing: ' . $cmd, Project::MSG_DEBUG);
+				@exec($cmd, $output, $return);
+
+				if ($return !== 0) {
+					$this->log("Failed processing ${file}!", Project::MSG_ERR);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return FileSet
+	 */
+	public function createFileSet()
+	{
+		$num = array_push($this->_fileSets, new FileSet);
+		return $this->_fileSets[$num - 1];
+	}
+
+	/**
+	 * @param PhingFile $path
+	 * @return void
+	 */
+	public function setJarPath(PhingFile $path)
+	{
+		$this->_jarPath = $path;
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function _checkJarPath()
+	{
+		if ($this->_jarPath === null) {
+			throw new BuildException(
+				'Path to YUI compressor jar file must be specified',
+				$this->location
+			);
+		} else if (!$this->_jarPath->exists()) {
+			throw new BuildException(
+				'Unable to locate jar file at specified path',
+				$this->location
+			);
+		}
+	}
+
+	/**
+	 * @param PhingFile $path
+	 * @return void
+	 */
+	public function setTargetDir(PhingFile $path)
+	{
+		$this->_targetDir = $path;
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function _checkTargetDir()
+	{
+		if ($this->_targetDir === null) {
+			throw new BuildException(
+				'Target directory must be specified',
+				$this->location
+			);
+		}
+	}
+
+}
